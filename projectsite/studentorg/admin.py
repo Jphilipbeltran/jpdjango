7197cc57a6b111c0ab6023a6efdceb96fb5c8e16
Johnphilipbeltran <141928294+Johnphilipbeltran@users.noreply.github.com>
2024-04-04 21:10:00 +0800

    models and admin working properly

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -6,3 +6,23 @@ admin.site.register(Program)
 admin.site.register(Organization)
 admin.site.register(Student)
 admin.site.register(Orgmember)
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("student_id", "lastname",  "firstname",      "middlename",   "program")
+    search_fields = ("last name", "firstname",)
+
+
+admin.site.register(Orgmember)
+@admin.register(Orgmember)
+class OrgMamberAdmin(admin.ModelAdmin):
+    list_display = ("student", "get_member_program",  "organization",      "date_joined",)
+    search_fields = ("student_last name",  "student_firstname",)
+
+    def get_member_program(self, obj):
+        try:
+            member=Student.objects.get(id=obj.student_id)
+            return member.program
+        except Student. DoesNotExist:
+            return None
